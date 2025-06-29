a527cb7faff6361d280df6932ac11e9e6337088f
Rahul Sharma <134831762+rahulsharma7777@users.noreply.github.com>
2025-06-29 12:08:14 +0530

    added  signup route

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,68 @@
 import { Hono } from "hono";
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { decode, sign, verify } from "hono/jwt";
 
-const app = new Hono();
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
 
-app.post("/api/vi/signup", (c) => {
-  return c.text("Hello Hono!");
+// Signup route
+const getPrisma = (env: any) =>
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: env.DATABASE_URL,
+      },
+    },
+  }).$extends(withAccelerate());
+
+app.post("/api/v1/signup", async (c) => {
+  const prisma = getPrisma(c.env);
+  const body = await c.req.json();
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    },
+  });
+
+  const token = await sign({ id: user.id }, c.env.JWT_SECRET);
+  return c.json({ jwt: token });
 });
-app.post("/api/vi/sigin", (c) => {
-  return c.text("Hello Hono!");
+
+// Signin route
+app.post("/api/v1/signin", async (c) => {
+  const getPrisma = (env: any) =>
+    new PrismaClient({
+      datasources: {
+        db: {
+          url: env.DATABASE_URL,
+        },
+      },
+    }).$extends(withAccelerate());
+  const prisma = getPrisma(c.env);
+
+  const body = await c.req.json();
+  const user = await prisma.user.findUnique({
+    where: { email: body.email },
+  });
+
+  if (!user) {
+    c.status(403);
+    return c.json({ error: "User Not Found" });
+  }
+
+  const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+  return c.json({ jwt });
 });
-app.post("/api/vi/blog", (c) => {
-  return c.text("Hello Hono!");
-});
-app.get("/api/vi/blog", (c) => {
-  return c.text("Hello Hono!");
-});
-app.get("/api/vi/blog/:blog", (c) => {
-  return c.text("Hello Hono!");
-});
+
+// Blog routes
+app.post("/api/v1/blog", (c) => c.text("Hello Hono!"));
+app.get("/api/v1/blog", (c) => c.text("Hello Hono!"));
+app.get("/api/v1/blog/:blog", (c) => c.text("Hello Hono!"));
+
 export default app;
