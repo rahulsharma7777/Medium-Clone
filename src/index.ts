f8eaf4041b18c793eb3c787e992fbdb36c12fea4
Rahul Sharma <134831762+rahulsharma7777@users.noreply.github.com>
2025-07-25 08:04:54 +0530

    completed blog routes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
 import { userRouter } from "./routes/users";
+import { blogRouter } from "./routes/blog";
 import { cors } from "hono/cors";
 
 const app = new Hono();
-app.use('/',cors());
+app.use('/*',cors());
 app.route('/api/v1/user',userRouter);
+app.route("/api/v!/blog",blogRouter)
 
 
 
--- src/routes/blog.ts
+++ src/routes/blog.ts
@@ -3,7 +3,6 @@ import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { verify } from "hono/jwt";
 import { z } from "zod";
-import errorMap from "zod/locales/en.js";
 const createBlogInput = z.object({
   title: z.string(),
   content: z.string(),
@@ -109,24 +108,127 @@ blogRouter.put("/uploadblog", async (c) => {
         error: "Invalid Input",
       });
     }
-    const {id ,title ,content}=body;
-     const blog=await prisma.blog.update({
-      where:{
-          id,
-          authorId:c.get('userId')
+    const { id, title, content } = body;
+    const blog = await prisma.blog.update({
+      where: {
+        id,
+        authorId: c.get("userId"),
       },
-        data:{
-             title,
-             content
-        }
-     });
-     return c.text("Blog Updated")
-
-
+      data: {
+        title,
+        content,
+      },
+    });
+    return c.text("Blog Updated");
   } catch (error) {
     c.status(400);
     return c.json({
-      error:"Error While updating blog"
-    })
+      error: "Error While updating blog",
+    });
+  }
+});
+blogRouter.get("/bulk", async (c) => {
+  try {
+    const getPrisma = (env: any) =>
+      new PrismaClient({
+        datasources: {
+          db: {
+            url: env.DATABASE_URL,
+          },
+        },
+      }).$extends(withAccelerate());
+    const prisma = getPrisma(c.env);
+
+    const blogs = await prisma.blog.findMany({
+      select: {
+        id: true,
+        title: true,
+        content: true,
+        published: true,
+        createdAt: true,
+        author: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
+
+    return c.json({
+      blogs,
+    });
+  } catch (error) {
+    c.status(400);
+    return c.json({
+      error: "Error While fetching blogs ",
+    });
+  }
+});
+blogRouter.get("/:id", async (c) => {
+  try {
+    const getPrisma = (env: any) =>
+      new PrismaClient({
+        datasources: {
+          db: {
+            url: env.DATABASE_URL,
+          },
+        },
+      }).$extends(withAccelerate());
+    const prisma = getPrisma(c.env);
+    const id = c.req.param("id");
+    const blog = await prisma.blog.findFirst({
+      where: {
+        id,
+      },
+      select: {
+        id: true,
+        title: true,
+        content: true,
+        published: true,
+        createdAt: true,
+        author: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
+    return c.json({
+      blog,
+    });
+  } catch (error) {
+    c.status(400);
+    return c.json({
+      json: "Error While fetching blog",
+    });
+  }
+});
+blogRouter.delete("/:id", async (c) => {
+  try {
+    const getPrisma = (env: any) =>
+      new PrismaClient({
+        datasources: {
+          db: {
+            url: env.DATABASE_URL,
+          },
+        },
+      }).$extends(withAccelerate());
+    const prisma = getPrisma(c.env);
+    const id = c.req.param("id");
+    await prisma.blog.delete({
+      where: {
+        id,
+        authorId: c.get("userId"),
+      },
+    });
+
+    return c.json({
+      msg: "Blog Deleted",
+    });
+  } catch (error) {
+    c.status(400);
+    return c.json({
+      error: "Error While deleting blog",
+    });
   }
 });
